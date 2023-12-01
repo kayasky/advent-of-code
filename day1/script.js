be635ff4d0e6d796ce144c4f9072bf0f14159d3e
Kaya Jhuti <itskayaj@gmail.com>
2023-12-01 14:46:45 -0500

    Finish part 1 of day 1

--- day1/script.js
+++ day1/script.js
@@ -1,9 +1,17 @@
 import input from `./input.json` assert { type: "json" };
 
+let sum = 0;
+
 function getCalibrationValue(value) {
-  return value;
+  const int = value.split(/ /)[0].replace(/[^\d]/g, '');
+  const intStr = int.toString();
+  let calibrationValue = 0;
+  if (!isNaN(int)) {
+    calibrationValue = `${intStr[0]}${intStr[intStr.length-1]}`;
+  }
+  return parseInt(calibrationValue);
 }
 
-input.values.forEach(value => {
-  console.log(getCalibrationValue(value));
-});
+input.values.forEach(value => { sum += getCalibrationValue(value)});
+
+console.log(sum);
