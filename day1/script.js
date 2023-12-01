9253d4734d4a30fcff1a39d0e0d26210108049a7
Kaya Jhuti <itskayaj@gmail.com>
2023-12-01 14:49:01 -0500

    Prep for part 2

--- day1/script.js
+++ day1/script.js
@@ -1,17 +1,25 @@
 import input from `./input.json` assert { type: "json" };
 
-let sum = 0;
+// part 1
+(() => {
+  let sum = 0;
 
-function getCalibrationValue(value) {
-  const int = value.split(/ /)[0].replace(/[^\d]/g, '');
-  const intStr = int.toString();
-  let calibrationValue = 0;
-  if (!isNaN(int)) {
-    calibrationValue = `${intStr[0]}${intStr[intStr.length-1]}`;
+  function getCalibrationValue(value) {
+    const int = value.split(/ /)[0].replace(/[^\d]/g, '');
+    const intStr = int.toString();
+    let calibrationValue = 0;
+    if (!isNaN(int)) {
+      calibrationValue = `${intStr[0]}${intStr[intStr.length-1]}`;
+    }
+    return parseInt(calibrationValue);
   }
-  return parseInt(calibrationValue);
-}
+  
+  input.values.forEach(value => { sum += getCalibrationValue(value)});
+  
+  console.log(sum);
+})();
 
-input.values.forEach(value => { sum += getCalibrationValue(value)});
-
-console.log(sum);
+// part 2
+(() => {
+ 
+})();
