e97a70fbb6b07a916b03285c88591ead62c086ac
Kaya Jhuti <itskayaj@gmail.com>
2023-12-02 21:16:10 -0500

    Get the number of each coloured cube

--- day2/script.js
+++ day2/script.js
@@ -1,6 +1,18 @@
 import input from `./input.json` assert { type: "json" };
 
-
 (() => {
-  console.log(input);
-})()
+  const MaxCubes = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  }
+  const games = input.values;
+  
+  games.forEach((game, index) => {
+    game.match(/[1-9][0-9]* red/g).forEach(cubes => console.log(cubes));
+    game.match(/[1-9][0-9]* green/g).forEach(cubes => console.log(cubes));
+    game.match(/[1-9][0-9]* blue/g).forEach(cubes => console.log(cubes));
+    console.log('+-----------------+')
+  });
+
+})();
