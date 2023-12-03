16e1e37324f8c218c2e06b8907a86e9c50cba949
Kaya Jhuti <itskayaj@gmail.com>
2023-12-02 21:29:24 -0500

    Possibly done with today's challenge?

--- day2/script.js
+++ day2/script.js
@@ -7,12 +7,21 @@ import input from `./input.json` assert { type: "json" };
     blue: 14,
   }
   const games = input.values;
-  
+  let sum = 0;
+
   games.forEach((game, index) => {
-    game.match(/[1-9][0-9]* red/g).forEach(cubes => console.log(cubes));
-    game.match(/[1-9][0-9]* green/g).forEach(cubes => console.log(cubes));
-    game.match(/[1-9][0-9]* blue/g).forEach(cubes => console.log(cubes));
-    console.log('+-----------------+')
+    const gameId = index + 1;
+    
+    const reds = game.match(/[1-9][0-9]* red/g).map(cubes => parseInt(cubes));
+
+    const greens = game.match(/[1-9][0-9]* green/g).map(cubes => parseInt(cubes));
+    
+    const blues = game.match(/[1-9][0-9]* blue/g).map(cubes => parseInt(cubes));
+
+    if (Math.max(...reds) <= MaxCubes.red && Math.max(...greens) <= MaxCubes.green && Math.max(...blues) <= MaxCubes.blue) {
+      sum += gameId;
+    }
   });
 
+  console.log(sum);
 })();
