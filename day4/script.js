da4c1df53725d0eeadda1b0ad88f3fec11ac86fb
Kaya Jhuti <itskayaj@gmail.com>
2023-12-04 14:44:17 -0500

    Clean up code and prep for part 2

--- day4/script.js
+++ day4/script.js
@@ -1,21 +1,35 @@
 const fs = require('fs');
 const input = fs.readFileSync('./day4/input.txt', 'utf8');
 
+// part 1
 (() => {
   const pileOfScratchCards = input.split('\n');
   let points = 0;
 
-  pileOfScratchCards.forEach((card) => {
-    const splitCard = card.split(' | ')
-    const winningNumbers = new Set(splitCard[0].substring(8).split(' ').filter((number) => number !== ''));
-    const myNumbers = new Set(splitCard[1].split(' ').filter((number) => number !== ''));
-    const myWinningNumbers = [...winningNumbers.intersection(myNumbers)]
+  const calculatePoints = (winningNumbers, myNumbers) => {
+    const myWinningNumbers = [...winningNumbers.intersection(myNumbers)];
     let pointsForThisCard = 0;
     if (myWinningNumbers.length) {
       pointsForThisCard = Math.pow(2, myWinningNumbers.length - 1);
     }
-    points += pointsForThisCard;
+    return pointsForThisCard;
+  };
+
+  const getSet = (cardNumbers) => {
+    return new Set(cardNumbers.split(' ').filter((number) => number !== ''));
+  }
+
+  pileOfScratchCards.forEach((card) => {
+    const splitCard = card.split(' | ');
+    const winningNumbers = getSet(splitCard[0].substring(8));
+    const myNumbers = getSet(splitCard[1]);
+    points += calculatePoints(winningNumbers, myNumbers);
   });
 
   console.log(`I won ${points} points!`);
+})();
+
+// part 2
+(() => {
+  
 })();
