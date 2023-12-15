1ff98a467663393cf0b575e27200693eeb6560ac
Kaya Jhuti <itskayaj@gmail.com>
2023-12-14 21:43:57 -0500

    Add logic to get all sequences in history

--- day9/script.js
+++ day9/script.js
@@ -2,5 +2,32 @@ const fs = require('fs');
 const input = fs.readFileSync('./day9/input.txt', 'utf8');
 
 (() => {
-  console.log(input);
+  const histories = input.split('\n');
+  const firstHistory = histories[2].split(' ').map(Number);
+  const allSequences = calculateAllSequences(firstHistory);
+
+  console.log(allSequences);
+
+  function calculateAllSequences(history) {
+    let sequences = [history];
+    calculateNextSequence(history);
+
+    function calculateNextSequence(history) {
+      let nextSequence = [];
+
+      for (let i = 1; i < history.length; i++) {
+        nextSequence.push(history[i] - history[i - 1]);
+        if (nextSequence.length === history.length - 1) {
+          sequences.push(nextSequence);
+          if (nextSequence.every(item => item === 0)) {
+            break;
+          } else {
+            calculateNextSequence(nextSequence);
+          }
+        }
+      }
+    }
+    return sequences;
+  }
+
 })();
