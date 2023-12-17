b0ae17935ac089671cd71ae988d4e10190a0ba92
Kaya Jhuti <itskayaj@gmail.com>
2023-12-17 15:47:47 -0500

    can still improve the code

--- day11/script.js
+++ day11/script.js
@@ -3,13 +3,16 @@ const input = fs.readFileSync('./day11/input.txt', 'utf8');
 
 (() => {
   let numberofGalaxies = 0;
-  const expansionFactor = 1000000;
+  const expansionFactor = 1000000; //set this to 2 for part 1
+  const inputGrid = input.split('\n').map(row => row.split(''));
+
   const expandedRows = [];
   const expandedColumns = [];
-  const inputGrid = input.split('\n').map(row => row.split(''));
   const expandedGrid = expandGrid(inputGrid);
-  console.log(expandedGrid.join('\n').replace(/,/g, ''));
-  let sumOfAllLengths = 0;
+  const allPairs = getAllPossiblePairs(numberofGalaxies);
+  const sumOfAllLengths = getSumOfAllLengths(allPairs, expandedGrid);
+
+  console.log(`The answer is: `, sumOfAllLengths);
 
   function expandGrid(inputGrid) {
     let outputGrid = expandGridVertically(inputGrid);
@@ -17,37 +20,33 @@ const input = fs.readFileSync('./day11/input.txt', 'utf8');
     return outputGrid;
   }
 
-  // all possible pairs:
-  let allPairs = [];
-  for (let i = 1; i <= numberofGalaxies; i++) {
-    for (let j = i + 1; j <= numberofGalaxies; j++) {
-      allPairs.push([i, j]);
-    }
+  function getSumOfAllLengths(pairsOfGalaxies, grid) {
+    let sumOfAllLengths = 0;
+
+    pairsOfGalaxies.forEach(pair => {
+      const galaxy1Coords = getCoordinatesOfGalaxy(grid, pair[0]);
+      const galaxy2Coords = getCoordinatesOfGalaxy(grid, pair[1]);
+
+      const numOfExpandedRowsInBetween = expandedRows.filter(row => row > Math.min(galaxy1Coords[0], galaxy2Coords[0]) && row < Math.max(galaxy1Coords[0], galaxy2Coords[0])).length;
+      const numOfExpandedColumnsInBetween = expandedColumns.filter(col => col > Math.min(galaxy1Coords[1], galaxy2Coords[1]) && col < Math.max(galaxy1Coords[1], galaxy2Coords[1])).length;
+
+      if (galaxy1Coords[0] > galaxy2Coords[0]) {
+        galaxy1Coords[0] += (numOfExpandedRowsInBetween * expansionFactor) - numOfExpandedRowsInBetween;
+      } else if (galaxy1Coords[0] < galaxy2Coords[0]) {
+        galaxy2Coords[0] += (numOfExpandedRowsInBetween * expansionFactor) - numOfExpandedRowsInBetween;
+      }
+
+      if (galaxy1Coords[1] > galaxy2Coords[1]) {
+        galaxy1Coords[1] += (numOfExpandedColumnsInBetween * expansionFactor) - numOfExpandedColumnsInBetween;
+      } else if (galaxy1Coords[1] < galaxy2Coords[1]) {
+        galaxy2Coords[1] += (numOfExpandedColumnsInBetween * expansionFactor) - numOfExpandedColumnsInBetween;
+      }
+
+      sumOfAllLengths += Math.abs(galaxy1Coords[0] - galaxy2Coords[0]) + Math.abs(galaxy1Coords[1] - galaxy2Coords[1]);
+    });
+
+    return sumOfAllLengths;
   }
-
-  allPairs.forEach(pair => {
-    const galaxy1Coords = getCoordinatesOfGalaxy(expandedGrid, pair[0]);
-    const galaxy2Coords = getCoordinatesOfGalaxy(expandedGrid, pair[1]);
-
-    const numOfExpandedRowsInBetween = expandedRows.filter(row => row > Math.min(galaxy1Coords[0], galaxy2Coords[0]) && row < Math.max(galaxy1Coords[0], galaxy2Coords[0])).length;
-    const numOfExpandedColumnsInBetween = expandedColumns.filter(col => col > Math.min(galaxy1Coords[1], galaxy2Coords[1]) && col < Math.max(galaxy1Coords[1], galaxy2Coords[1])).length;
-
-    if (galaxy1Coords[0] > galaxy2Coords[0]) {
-      galaxy1Coords[0] += (numOfExpandedRowsInBetween * expansionFactor) - numOfExpandedRowsInBetween;
-    } else if (galaxy1Coords[0] < galaxy2Coords[0]) {
-      galaxy2Coords[0] += (numOfExpandedRowsInBetween * expansionFactor) - numOfExpandedRowsInBetween;
-    }
-
-    if (galaxy1Coords[1] > galaxy2Coords[1]) {
-      galaxy1Coords[1] += (numOfExpandedColumnsInBetween * expansionFactor) - numOfExpandedColumnsInBetween;
-    } else if (galaxy1Coords[1] < galaxy2Coords[1]) {
-      galaxy2Coords[1] += (numOfExpandedColumnsInBetween * expansionFactor) - numOfExpandedColumnsInBetween;
-    }
-
-    sumOfAllLengths += Math.abs(galaxy1Coords[0] - galaxy2Coords[0]) + Math.abs(galaxy1Coords[1] - galaxy2Coords[1]);
-  });
-
-  console.log(sumOfAllLengths);
 
   function getCoordinatesOfGalaxy(grid, galaxyName) {
     const galaxyXCoord = grid
@@ -62,8 +61,7 @@ const input = fs.readFileSync('./day11/input.txt', 'utf8');
     let outputGrid = [];
     for (let i = 0; i < inputGrid.length; i++) {
       outputGrid.push(inputGrid[i]);
-      if (inputGrid[i].every(element => element === '.')) {
-        //outputGrid.push(inputGrid[i].map(() => '.'));
+      if (inputGrid[i].every(element => element === '.') && expandedRows.indexOf(i) === -1) {
         expandedRows.push(i);
       }
     }
@@ -90,14 +88,20 @@ const input = fs.readFileSync('./day11/input.txt', 'utf8');
     return galaxyCount;
   }
   function insertSpaceInColumn(outputGrid, col, isColumnEmpty) {
-    if (isColumnEmpty) {
+    if (isColumnEmpty && expandedColumns.indexOf(col) === -1) {
       expandedColumns.push(col);
-      // for (let k = 0; k < outputGrid.length; k++) {
-      //   outputGrid[k].splice(col, 0, '.');
-      // }
-      // col += 1;
     }
     return col;
+  }
+
+  function getAllPossiblePairs(numberofGalaxies) {
+    const allPairs = [];
+    for (let i = 1; i <= numberofGalaxies; i++) {
+      for (let j = i + 1; j <= numberofGalaxies; j++) {
+        allPairs.push([i, j]);
+      }
+    }
+    return allPairs;
   }
 
 })();
